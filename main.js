170832d88c10b222a5c907e8de2a5e1e224f5cef
maxp421 <penzes.max@gmail.com>
2023-10-06 13:26:11 +0200

    added grid

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const canvas = document.querySelector('#canvas');
+//GENERATE CUSTOM EVENT for both mousedown and mouseover simultaneously?
+canvas.addEventListener('drag', paint);
+function paint(e) {
+  if (!e.currentTarget.contains(e.target)) return;
+  if (e.target.tagName !== 'TD') return;
+  console.log(e.target.tagName);
+}
+
+for (let i = 0; i < 16; i++) {
+  let tr = document.createElement('tr');
+  canvas.append(tr);
+  for (let j = 0; j < 16; j++) {
+    let td = document.createElement('td');
+    tr.append(td);
+  }
+}
+
